0e5111a822f794709c63c366740c58df8489ec5c
GHOST <ghostdevbusiness@gmail.com>
2024-06-30 15:35:19 +0000

    init

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,5 @@
+import { EXAMPLE_B } from '$env/static/private';
+
+export async function load() {
+    return { EXAMPLE_B }
+}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { PUBLIC_EXAMPLE_A } from '$env/static/public';
+
+	export let data;
+</script>
+
+<p>Public var: "{PUBLIC_EXAMPLE_A}"</p>
+<p>Private var: "{data.EXAMPLE_B}"</p>
